5aaca24421a88e3dbc82c3de3719256aa9b95e9d
MartinLopezDeIpina <mlopezdeipina001@ikasle.ehu.eus>
2024-05-15 02:31:05 +0200

    feat layout de info de perfil, no funciona

---
+++ app/src/main/java/com/lksnext/parkingplantilla/view/adapter/ProfileInfoAdapter.java
@@ -0,0 +1,14 @@
+package com.lksnext.parkingplantilla.view.adapter;
+
+import android.graphics.drawable.Drawable;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.databinding.BindingAdapter;
+
+import com.lksnext.parkingplantilla.R;
+
+public class ProfileInfoAdapter {
+
+}
