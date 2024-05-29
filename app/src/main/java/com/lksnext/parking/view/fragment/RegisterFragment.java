a0372bc74e0eeceef8f4528428149236b6147a65
MartinLopezDeIpina <mlopezdeipina001@ikasle.ehu.eus>
2024-05-30 00:48:35 +0200

    feat Login activity now uses FragmentContainerview

--- app/src/main/java/com/lksnext/parking/view/fragment/RegisterFragment.java
+++ app/src/main/java/com/lksnext/parking/view/fragment/RegisterFragment.java
@@ -4,19 +4,26 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import com.lksnext.parking.R;
 import com.lksnext.parking.databinding.FragmentProfileBinding;
+import com.lksnext.parking.databinding.FragmentRegisterBinding;
 import com.lksnext.parking.viewmodel.MainViewModel;
 
 public class RegisterFragment extends Fragment {
 
-        private FragmentProfileBinding binding;
+        private FragmentRegisterBinding binding;
         public RegisterFragment() {
             // Es necesario un constructor vacio
         }
@@ -24,6 +31,18 @@ public class RegisterFragment extends Fragment {
         @Override
         public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                                  @Nullable Bundle savedInstanceState) {
-                return inflater.inflate(R.layout.fragment_register, container, false);
+
+                binding = com.lksnext.parking.databinding.FragmentRegisterBinding.inflate(inflater, container, false);
+
+                bindReturnButton();
+
+                return binding.getRoot();
         }
+        private void bindReturnButton(){
+                binding.returning.setOnClickListener(v -> {
+                        NavController navController = Navigation.findNavController(v);
+                        navController.navigate(R.id.login_fragment);
+                });
+        }
+
 }
