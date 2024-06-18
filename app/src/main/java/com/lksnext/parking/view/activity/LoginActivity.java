152859cf2e74a995eeda68cdae3f2dd116a546e0
MartinLopezDeIpina <mlopezdeipina001@ikasle.ehu.eus>
2024-06-18 23:12:41 +0200

    feat desactivar chips de horas cuando no hay tipo de plaza o fecha seleccionada

--- app/src/main/java/com/lksnext/parking/view/activity/LoginActivity.java
+++ app/src/main/java/com/lksnext/parking/view/activity/LoginActivity.java
@@ -37,7 +37,7 @@ public class LoginActivity extends BaseActivity {
         DataBaseFiller dataBaseFiller = new DataBaseFiller();
         //dataBaseFiller.fillReservas();
         //dataBaseFiller.fillPlaza98();
-        dataBaseFiller.fillPlazas75_94();
+        //dataBaseFiller.fillPlazas75_94();
 
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
