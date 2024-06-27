b2ec12424ba142ceaf72d4c96079799ba4bc6481
MartinLopezDeIpina <mlopezdeipina001@ikasle.ehu.eus>
2024-06-27 18:00:51 +0200

    feat confirmar email funciona

--- app/src/main/java/com/lksnext/parking/view/fragment/ConfirmEmailFragmnet.java
+++ app/src/main/java/com/lksnext/parking/view/fragment/ConfirmEmailFragmnet.java
@@ -4,17 +4,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import com.lksnext.parking.R;
 import com.lksnext.parking.databinding.FragmentConfirmEmailBinding;
 import com.lksnext.parking.viewmodel.RegisterViewModel;
 
 public class ConfirmEmailFragmnet extends Fragment {
     private FragmentConfirmEmailBinding binding;
+    private ImageView botonAtras;
+    private Button botonAceptar;
+    private NavController navController;
     public ConfirmEmailFragmnet() {
         // Es necesario un constructor vacio
     }
@@ -25,6 +33,25 @@ public class ConfirmEmailFragmnet extends Fragment {
 
         binding = com.lksnext.parking.databinding.FragmentConfirmEmailBinding.inflate(inflater, container, false);
 
+
+        bindButtonAtras();
+        bindButtonAceptar();
         return binding.getRoot();
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+    }
+
+    private void bindButtonAtras() {
+        botonAtras = binding.returning;
+        botonAtras.setOnClickListener(v -> navController.navigate(R.id.login_fragment));
+    }
+    private void bindButtonAceptar() {
+        botonAceptar = binding.aceptarButton;
+        botonAceptar.setOnClickListener(v -> navController.navigate(R.id.login_fragment));
+    }
+
 }
